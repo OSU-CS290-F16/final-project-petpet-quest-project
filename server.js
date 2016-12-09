3414edc6d7bd10f01c281415cf7fff7509f214b4
Connor Stettler <stettlec@flip3.engr.oregonstate.edu>
2016-12-08 23:35:17 -0800

    Adding MongoDB test to server.js

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 44454;
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+
+var url = 'mongodb://cs290_stettlec:U5WD3mGuB75pzfW@classmongo.engr.oregonstate.edu/cs290_stettlec';
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 
 app.get('/', function (req, res) {
@@ -24,3 +31,46 @@ app.get('*', function(req, res) {
 app.listen(port, function () {
   console.log("== Listening on port", port);
 });
+
+
+
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('serious');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('serious');
+  // Find some documents
+  collection.find({'a': 3}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+/*
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  insertDocuments(db, function() {
+    findDocuments(db, function() {
+      db.close();
+    });
+  });
+});
+*/
