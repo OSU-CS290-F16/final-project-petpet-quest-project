a7088af88442ed47cdefb5c6838a26e037829873
kiefk <kiefk@flip3.engr.oregonstate.edu>
2016-12-08 21:30:51 -0800

    added the ability to buy the first pet

--- public/shop.js
+++ public/shop.js
@@ -1,16 +1,25 @@
 //javascript for interaction with the shop page
 
-function sold(i){
+function sold(){
+		console.log("I am in sold");
+		console.log("i = %d", i);
+		
 		var value = 1000 + (i*500);
 		
 		var pet; 
 		
-		if(i == 0){
+		i += 1;
+		 
+		if(price[0]){
+			console.log("sold");
 			pet = document.getElementById("pet1");
 			pet.style.display = "block";
 			pet.style.marginTop = "-331px";
-			pet.style.marginLeft = "54px";
+			pet.style.marginLeft = "53px";
+			price[0].removeEventListener("click", sold);
 		}
+		
+		return false;
 }
 
 
@@ -19,8 +28,7 @@ var price = document.getElementsByClassName("Price"); //select the price button 
 for(var i = 0; i< price.length; i++){
 	//if the price of a pet is clicked and there is enough money to buy
 	//user will buy pet and the amount will be taken out of their points
-	
-	price[i].addEventListener("click", sold(i));
+	price[i].addEventListener("click", sold);
 }
 
 
