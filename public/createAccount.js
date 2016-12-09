0a855850ff13e72caa4c5b5b2a661de2aa147abf
Connor Stettler <stettlec@flip1.engr.oregonstate.edu>
2016-12-09 11:43:51 -0800

    Added minor changes to createAccount.js and .html. Updated server to include more user info

---
+++ public/createAccount.js
@@ -0,0 +1,15 @@
+
+
+// $.ajax({
+  // method: "POST",
+  // url: "/createAccount",
+  // data: { user: "John", pass: "Boston" }
+  // .done(function() {
+    // alert( "success" );
+  // })
+  // .fail(function() {
+    // alert( "error" );
+  // })
+// })
+
+alert($);
