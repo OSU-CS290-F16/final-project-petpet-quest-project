e03974193b79728c3c39cab96a228e86a86061c2
Connor Stettler <stettlec@flip1.engr.oregonstate.edu>
2016-12-09 11:01:38 -0800

    Updated server to accept registration posts. Updated package.json

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 33995;
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 42995;
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
@@ -16,6 +17,11 @@ var mongoDB;
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+
 app.get('/', function (req, res) {
 
   res.send(indexhtml);
@@ -27,6 +33,69 @@ app.get('*', function(req, res) {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
+app.post('/createAccount', function(req, res){
+		// var myCursor = mongoDB.collection('users').find({"username" : "Nate", "password" : "natepass"});
+		// var documentArray = myCursor.toArray();
+		// console.log("Is:  " + documentArray[0])
+		//console.log(mongoDB.collection('users').find({"Username" : "Nate", "password" : "natepass"}));
+		//console.log(mongoDB.collection('users').find({"username" : "Nate"}));
+		
+		// console.log("Should be: " + req.body.user);
+		// var myCursor = mongoDB.collection('users').find({"username" : req.body.user, "password" : req.body.pass});
+		// var documentArray = myCursor.toArray();
+		// var myDocument = documentArray[1];
+		// console.log("Is:  " + documentArray[0])
+		
+		//console.log(req.body.user + ' ' + req.body.pass);
+		//console.log(mongoDB.collection('users').find({"username" : req.body.user}));
+	if ( typeof req.body.user === 'undefined' || typeof req.body.pass === 'undefined') {
+		res.status(400);
+		console.log('incorrect! Missing user or pass.')
+		res.send('Missing user or pass');
+		res.end();
+	}
+	mongoDB.collection('users').find({"username": req.body.user}).toArray(function(err, docs) {
+	assert.equal(err, null);
+	
+	if (docs.length === 0) {
+		mongoDB.collection('users').insert(
+		{"username" : req.body.user, "pass" : req.body.pass}
+		);
+		console.log("Tried to inset into database users")
+		console.log('making new account');
+		res.status(200);
+		res.send('Making new account');
+		res.end();
+	}
+	
+	else {
+		console.log(docs[0].username);
+		console.log(req.body.user);
+		console.log("User already exists");
+		res.status(400);
+		res.send('User already exists');
+		res.end();
+	}
+	});
+	
+
+
+	/*
+	mongoDB.collection('users').insert(
+	{"Username" : "Nate", "Pass" : "natepass"}
+	);
+	console.log("Tried to inset into database users");*/
+});
+
+
+/*
+app.post('/check', function(req, res){
+	if(mongoDB.collection('users').find(
+	{"Username" : "Nate"}
+	);) {
+	console.log("Tried to for existence of Nate");
+	}
+});*/
 
 
 var insertDocuments = function(db, callback) {
